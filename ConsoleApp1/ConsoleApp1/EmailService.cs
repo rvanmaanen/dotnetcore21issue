a0bcf8742ee94cce07a82b0a988d7bcc693b0ab0
Reinier van Maanen <rvanmaanen@xpirit.com>
2018-04-24 12:08:56 +0200

    Added project to demo issue

---
+++ ConsoleApp1/ConsoleApp1/EmailService.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    public interface IEmailService
+    {
+
+    }
+
+    public class EmailServiceClient : System.ServiceModel.ClientBase<IEmailService>, IEmailService
+    {
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var berichtService = new EmailServiceClient();
+
+            berichtService.Endpoint.EndpointBehaviors.Add(new CustomBehavior());
+        }
+    }
+}
